b70e566dfe53697d7d27413182ae7a3d9c139b9b
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-15 20:34:27 +0000

    URL Shortening part 1 completed

--- express_server.js
+++ express_server.js
@@ -1,22 +1,39 @@
+function generateRandomString() {
+  let string = Math.random().toString(36).substring(7);
+  return string;
+  };
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
-res.render("urls_index", templateVars)
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-res.render("urls_show", templateVars)
+  res.render("urls_show", templateVars)
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase};
+res.render("urls_index", templateVars)
 });
 
 app.get("/", (req, res) => {
@@ -26,6 +43,7 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
