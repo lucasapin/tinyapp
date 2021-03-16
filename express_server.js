7df7978baf84b00293b2e48f49617bb01d64bb26
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-16 21:51:02 +0000

    commit after delete and edit url options

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+//Create a Function that generates a random 6-char long string
 function generateRandomString() {
   let string = Math.random().toString(36).substring(2,8);
   return string;
@@ -8,15 +9,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
-
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Get request to see the create new tinyURL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -25,6 +27,12 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);  
+});
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const urlToRedirect = req.params.shortURL;
+  urlDatabase[urlToRedirect] = req.body.longURL; 
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
