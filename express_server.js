ab715af698ca67f12ffcfacfc63fb156a00950ec
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-03-17 22:11:46 +0000

    commit before implementation of basic permission features

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 //Create a Function that generates a random 6-char long string
-function generateRandomString() {
+const generateRandomString = function() {
   let string = Math.random().toString(36).substring(2,8);
   return string;
-  };
+};
 
 const express = require("express");
 const app = express();
@@ -20,78 +20,78 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const usersDatabase = { 
+const usersDatabase = {
   "b2xVn2": {
     id: "b2xVn2",
     email: "user@example.com",
     password: "test"
   },
- "9sm5xK": {
+  "9sm5xK": {
     id: "9sm5xK",
     email: "user2@example.com",
     password: "test2"
   }
-}
+};
 
 //Get request to see the create new tinyURL page
 app.get("/urls/new", (req, res) => {
-  const user = usersDatabase[req.cookies["user_id"]]
+  const user = usersDatabase[req.cookies["user_id"]];
   const templateVars = {
-    user_id: req.cookies["user_id"], user}
+    user_id: req.cookies["user_id"], user};
   res.render("urls_new", templateVars);
 });
 
 //render the register page with the form
 app.get("/register", (req, res) => {
-  const user = usersDatabase[req.cookies["user_id"]]
+  const user = usersDatabase[req.cookies["user_id"]];
   const templateVars = {
-    user_id: req.cookies["user_id"], user}
-  res.render("user_registration", templateVars)
+    user_id: req.cookies["user_id"], user};
+  res.render("user_registration", templateVars);
 });
 //helper function to validate the user
 const emailLookup = (user, userDatabase) => {
-  const {email} = user
+  const {email} = user;
   for (const key in userDatabase) {
     if (email === userDatabase[key].email) {
-      return userDatabase[key]
+      return userDatabase[key];
     }
   }
-  return false
-}
+  return false;
+};
 
 // handler for the registration form
 app.post("/register", (req, res) => {
-  newUser = {email, password} = req.body
+  const newUser = {email, password} = req.body;
 
   if (emailLookup(newUser, usersDatabase)) {
     res.status(400).send('Email already exists!');
   } else {
-    id = generateRandomString()
-    usersDatabase[id] = {id, email, password}
-    res.cookie('user_id', id)
-    res.redirect('/urls')
+    const id = generateRandomString();
+    usersDatabase[id] = {id, email, password};
+    res.cookie('user_id', id);
+    res.redirect('/urls');
   }
-})
+});
 
 app.get("/login", (req, res) => {
-  const user = usersDatabase[req.cookies["user_id"]]
+  const user = usersDatabase[req.cookies["user_id"]];
   const templateVars = { user_id: req.cookies["user_id"], user};
-  res.render("login_form", templateVars)
-})
+  res.render("login_form", templateVars);
+});
 
 //Handle a POST request to /login -->
 app.post("/login", (req, res) =>{
-  const user = emailLookup(req.body, usersDatabase)
-  if(!user) {
-    res.status(403).send("User not found!")
+  const user = emailLookup(req.body, usersDatabase);
+  if (!user) {
+    res.status(403).send("User not found!");
   } else {
-  if(user.password === req.body.password) {
-    res.cookie("user_id", user.id).redirect('/urls')
-  } else {
-    res.status(403).send("Wrong password")
+    if (user.password === req.body.password) {
+      res.cookie("user_id", user.id).redirect('/urls');
+    } else {
+      res.status(403).send("Wrong password");
+    }
   }
-}
-})
+});
 
 // create the logout logic
 app.post("/logout", (req, res) =>{
@@ -102,12 +102,12 @@ app.post("/logout", (req, res) =>{
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);  
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   const urlToRedirect = req.params.shortURL;
-  urlDatabase[urlToRedirect] = req.body.longURL; 
+  urlDatabase[urlToRedirect] = req.body.longURL;
   res.redirect("/urls");
 });
 
@@ -123,15 +123,15 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const user = usersDatabase[req.cookies["user_id"]]
+  const user = usersDatabase[req.cookies["user_id"]];
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user_id: req.cookies["user_id"], user};
-  res.render("urls_show", templateVars)
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const user = usersDatabase[req.cookies["user_id"]]
+  const user = usersDatabase[req.cookies["user_id"]];
   const templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"], email: req.cookies["email"], user};
-res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.get("/", (req, res) => {
